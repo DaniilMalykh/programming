b5f3fad229d8a1a34498c90ebab519d8e035fe91
Felix Freidman <>
2019-10-17 22:23:25 +0800

    Сделал 1 4 5 задания, с остальным сосу лапу

--- Lesson8C/Lesson8C/main.cpp
+++ Lesson8C/Lesson8C/main.cpp
@@ -6,26 +6,77 @@ const double MATH_EXP = 2.71828;
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    double i;
-    double y1, y2, y3;
-    for(i = -5; i <= 5; i += 0.1) {
-//        y1 = pow(i, 4); y = x^4
+    /*Задание 1*/
+//    double i; //счетчик для функции
+//    double y1, y2, y3; //значения функции для проверки четности
+//    int j = 0; //счетчик для массива
+//    int sumOfValues = 0; //с помощью него мы проверяем контрольную сумму четности
+//    int values[111]; // массив будущих значений функции
+//    for(i = -5; i <= 5; i += 0.1) {
+//        while(j < 111) {
+//        y1 = pow(i, 4); //y = x^4
 //        y2 = pow(-i, 4);
 //        y3 = -1 * pow(i, 4);
-//        y1 = tan(i * (MATH_PI/180)); y = tg(x)
+    
+//        y1 = tan(i * (MATH_PI/180)); //y = tg(x)
 //        y2 = tan(-i * (MATH_PI/180));
 //        y3 = -1 * tan(i * (MATH_PI/180));
-//        y1 = pow(MATH_EXP, i); y = e^x
+    
+//        y1 = pow(MATH_EXP, i); //y = e^x
 //        y2 = pow(MATH_EXP, -i);
 //        y3 = -1 * pow(MATH_EXP, i);
-        if(y1 == y2){
-            cout << "Четная" << endl;
-        } else if(y2 == y3) {
-            cout << "Нечетная" << endl;
-        } else {
-            cout << "Общего вида" << endl;
-        }
-    }
+//
+//        if(y1 == y2){ // стандартная проверка четности
+//            values[j] = 0;
+//        } else if(y2 == y3) {
+//            values[j] = 1;
+//        } else {
+//            values[j] = 2;
+//        }
+//        sumOfValues = sumOfValues + values[j]; // подсчитываем контрольную сумму
+//            j++;
+//
+//    }
+//    }
+//
+//    if(sumOfValues == 0) {
+//        cout << "Функция четная на данном промежутке" << endl;
+//    } else if(sumOfValues == 111) {
+//        cout << "Функция нечетная на данном промежутке" << endl;
+//    } else {
+//        cout << "Функция общего вида на данном промежутке" << endl;
+//    }
+//
+//
+        
+//    }
+//    /*Задание 5*/
+//    double miles = 0;
+//    double fromMilesToKm;
+//    cout <<  "Из Мили в километры" << endl;
+//    cout << "Мили \t" << "Километры \t" << endl;
+//    for(int i = 0; i < 5; i++) {
+//        cin >> miles;
+//        fromMilesToKm = miles * 1.609344;
+//        cout << miles << "\t" << fromMilesToKm << endl;
+//    }
+    
+    /*Задание 4*/
+//    int creditMoney, persentagePerYear, incomePerYear, yearsWithCredit, persentageInMoney;
+//    yearsWithCredit = 0;
+//    cout << "Введите значение кредита" << endl;
+//    cin >> creditMoney;
+//    cout << "Введите % в год" << endl;
+//    cin >> persentagePerYear;
+//    cout << "Введите доход в год" << endl;
+//    cin >> incomePerYear;
+//    while(creditMoney > 0) {
+//        persentageInMoney = (creditMoney - incomePerYear) * (10 / 100);
+//        creditMoney = (creditMoney-incomePerYear) + persentageInMoney;
+//        yearsWithCredit++;
+//    }
+//    cout << yearsWithCredit << endl;
     
     return 0;
 }
+
